4bb4cd52590726f547e9234074b53687518c06ef
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-07-22 01:37:42 +0900

    Bump version for sequential action animation

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const DATA = {
-    version: '1.9.0',
+    version: '1.10.0',
     title: 'FGO バトルシミュレーター',
     classNames: {
       saber: 'セイバー', archer: 'アーチャー', lancer: 'ランサー',
