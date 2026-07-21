260ecf810ce5425104f59093e98b700d61c2af61
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-07-21 13:22:43 +0900

    Bump version for Extra Attack trigger fix

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const DATA = {
-    version: '1.5.0',
+    version: '1.5.1',
     title: 'FGO バトルシミュレーター',
     classNames: {
       saber: 'セイバー', archer: 'アーチャー', lancer: 'ランサー',
