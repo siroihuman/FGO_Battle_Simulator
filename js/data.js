31e3d77a5efada9d64f777df5867a76c4b7dab96
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-07-21 13:54:56 +0900

    Bump version for Order Change position fix

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const DATA = {
-    version: '1.5.1',
+    version: '1.5.2',
     title: 'FGO バトルシミュレーター',
     classNames: {
       saber: 'セイバー', archer: 'アーチャー', lancer: 'ランサー',
