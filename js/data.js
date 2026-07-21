2c1974d95e7eb01866b017c31567a3897a6d425a
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-07-21 22:19:50 +0900

    Bump version to v1.8.2

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const DATA = {
-    version: '1.8.1',
+    version: '1.8.2',
     title: 'FGO バトルシミュレーター',
     classNames: {
       saber: 'セイバー', archer: 'アーチャー', lancer: 'ランサー',
