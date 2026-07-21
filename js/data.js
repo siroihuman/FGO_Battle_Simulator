352e6a048e45b95f34ff5e48eefa13dd8e7b1946
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-07-21 22:01:57 +0900

    Bump version for full-screen turn resolution

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const DATA = {
-    version: '1.8.0',
+    version: '1.8.1',
     title: 'FGO バトルシミュレーター',
     classNames: {
       saber: 'セイバー', archer: 'アーチャー', lancer: 'ランサー',
