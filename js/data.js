8dff82671b5639728b89884a5263d3216ad9ba9e
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-07-22 20:14:49 +0900

    Bump version for revised class affinities

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const DATA = {
-    version: '1.11.0',
+    version: '1.11.1',
     title: 'FGO バトルシミュレーター',
     classNames: {
       saber: 'セイバー', archer: 'アーチャー', lancer: 'ランサー',
