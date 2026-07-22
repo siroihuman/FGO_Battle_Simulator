f859c4c182caaf08c5fc4ff3f11a5a1249563eca
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-07-22 23:24:22 +0900

    Bump version for enemy class defaults

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const DATA = {
-    version: '1.11.1',
+    version: '1.12.0',
     title: 'FGO バトルシミュレーター',
     classNames: {
       saber: 'セイバー', archer: 'アーチャー', lancer: 'ランサー',
