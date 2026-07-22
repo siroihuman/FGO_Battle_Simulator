bee6b5af6006fc842d679424a7d3ba87c0efc4df
siroihuman <siro_0626_2982_1998@yahoo.co.jp>
2026-07-22 22:36:24 +0900

    Register Beast No.031 mechanics

--- js/unique-mechanics/index.js
+++ js/unique-mechanics/index.js
@@ -4,5 +4,6 @@ const registry = require('./registry.js');
 require('./alice-liddell.js');
 require('./baphomet.js');
 require('./rlyeh.js');
+require('./beast-031.js');
 
 module.exports = registry;
